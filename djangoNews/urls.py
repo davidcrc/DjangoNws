ffb14557f5062ca09ab6b532f576d40030887966
david <david_drcr@hotmail.com>
2018-12-24 13:50:45 -0500

    Check templates y paso de datos a ellos

--- djangoNews/urls.py
+++ djangoNews/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+# from news import views      # traer la url que creamos
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # url(r'^index/', views.index),
+    url(r'^news/', include('news.urls')),   # asi mejor pa que sea modular y lo busque en ese paquete
 ]
