3a8f98cf27bab20fd4a3e9ca0975c05282d6bd05
david <david_drcr@hotmail.com>
2018-12-24 16:48:36 -0500

    Modificaciones a la migraciones de la BD (add)

--- news/models.py
+++ news/models.py
@@ -3,7 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Cada clase representa una tabla
+
+
 class Post(models.Model):
+    '''
+    Cada atributo representa una collumna , parece por defecto el ID
+    hay que checar por que aveces necesita que los parametros sean null al llenar la BD
+    '''
     title = models.CharField(max_length=150)
     body = models.TextField()
+    image = models.ImageField(upload_to="post_image", null = True )
+    create_at = models.DateTimeField(auto_now=True, auto_now_add=False, null = True )
+    update_at = models.DateTimeField(auto_now=False, auto_now_add=True, null = True )
