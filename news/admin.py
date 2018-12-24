ad4c5eef2184896710beb0589d664c6067f39cae
david <david_drcr@hotmail.com>
2018-12-24 17:35:54 -0500

    Activamos el administrador o dispatcher , pero aun me fala ver bm eso

--- news/admin.py
+++ news/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from models import Post
 # Register your models here.
+
+# Para que el modelo aparezaca en el sito de administracio
+# de django , debe añadirse aqui
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
