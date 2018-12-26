da2573796667615776ee6b402715dd830c0f9cd8
david <david_drcr@hotmail.com>
2018-12-26 09:57:58 -0500

    Crearon el objeto author & change las vars que son visbls en authr y Pos

--- news/admin.py
+++ news/admin.py
@@ -2,13 +2,20 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Post
+from models import Post, Author
 # Register your models here.
 
 # Para que el modelo aparezaca en el sito de administracio
 # de django , debe añadirse aqui
-
+@admin.register(Post)       # un decorador
 class PostAdmin(admin.ModelAdmin):
+    # Otra forma de retornar para que aparexca en el titulo
+    list_display = ('title', 'image_tag' ,'create_at', 'update_at')
+
+# admin.site.register(Post, PostAdmin)      # = q el @admin
+
+# TYambien añadir el author para que aparezca en el manager de django
+class AuthorAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
--- news/models.py
+++ news/models.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils.html import format_html
 from django.db import models
 
 # Cada clase representa una tabla
+
 
 
 class Post(models.Model):
@@ -14,5 +15,26 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     image = models.ImageField(upload_to="post_image", null = True )
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, null = True )
     create_at = models.DateTimeField(auto_now=True, auto_now_add=False, null = True )
     update_at = models.DateTimeField(auto_now=False, auto_now_add=True, null = True )
+
+    def image_tag(self):
+        return format_html( "<image src='{}' />".format(self.image.url))
+
+class Author(models.Model):
+    '''
+    
+    '''
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    
+    image = models.ImageField(upload_to="author_image", null = True )
+    create_at = models.DateTimeField(auto_now=True, auto_now_add=False, null = True )
+    update_at = models.DateTimeField(auto_now=False, auto_now_add=True, null = True )
+
+    # Cambiar el nombre dentro del objeto en el manage de django
+    def __unicode__(self):
+        return "%s %s" % (self.first_name, self.last_name)    
+
+
